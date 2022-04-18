9c8cad175076b19ccce4111c3af2fb6b76afc928
Kevin RodBe <kevin.rodriguez992@gmail.com>
2022-04-17 22:31:57 -0500

    feat(uy): uyt
    iuy

--- .versionrc.js
+++ .versionrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  changelogHeader: `# Hi\n\n bais bla bla hi hi hi 🔥 ![Insomnia API Client](https://raw.githubusercontent.com/Kong/insomnia/develop/screenshots/main.png)`,
+  changelogHeader: `# Hi\n\n bais bla bla hi hi🔥 ![Insomnia API Client](https://raw.githubusercontent.com/Kong/insomnia/develop/screenshots/main.png)`,
   releaseCommitMessageFormat: 'chore(release): {{currentTag}} [skip ci]',
   types: [
     { type: 'chore', section: 'Others', hidden: false },
